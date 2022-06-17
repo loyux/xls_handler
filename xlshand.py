db7a6e54ac49325c14be50b88135377eb039637f
loyu <lito0210@outlook.com>
2022-06-17 09:10:07 +0000

    delete unuseful code

--- xlshand.py
+++ xlshand.py
@@ -82,30 +82,6 @@ def write2excel(conn):
     workbook.save("output.xls")
 
 
-
-def transform(parent_path,out_path):
-    '''将xlsx转换为xls'''
-    fileList = os.listdir(parent_path)  #文件夹下面所有的文件
-    num = len(fileList)
-    for i in range(num):
-        file_Name = os.path.splitext(fileList[i])   #文件和格式分开
-        if file_Name[1] == '.xlsx':
-            transfile1 = parent_path+'\\'+fileList[i]  #要转换的excel
-            transfile2 = out_path+'\\'+file_Name[0]    #转换出来excel
-            excel=win32.gencache.EnsureDispatch('excel.application')
-            pro=excel.Workbooks.Open(transfile1)   #打开要转换的excel
-            pro.SaveAs(transfile2+".xls", FileFormat=56)  #另存为xls格式
-            pro.Close()
-            excel.Application.Quit()
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     conn = sqlite3.connect(":memory:")
     file_list = os.listdir("./")
